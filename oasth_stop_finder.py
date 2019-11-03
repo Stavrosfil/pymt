696e8bd5cd45c38fd42764338e4e0ac3428bc2a9
Stavros Filosidis <stavrosfil@gmail.com>
2019-11-03 03:48:52 +0200

    Write all the stops found, in a file

--- oasth_stop_finder.py
+++ oasth_stop_finder.py
@@ -1,15 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+# import time
 
+f = open("oasth_stops.txt", "a")
 
 # http://m.oasth.gr/index.php?md=4&sn=3&start=819&sorder=1&rc=2282&line=620&dir=1&ref=1
 url = "http://m.oasth.gr/index.php"
 # reqdata = {"md": 4, "sn": 3, "start": 820,
 #            "sorder": 1, "rc": 2282, "line": 620, "dir": 1}
 
-arivals = None
-counter = 819
+counter = 0
 
 session = requests.Session()
 
@@ -24,7 +24,12 @@ while True:
     # print(soup.prettify())
 
     bus_stop = soup.find('div', attrs={'class': 'title'}).text
-    print(bus_stop)
+    print(str(counter) + " \"" + bus_stop + "\"")
+
+    if(bus_stop != " "):
+        f.write(str(counter) + " " + bus_stop + '\n')
 
     counter += 1
     # time.sleep(1)
+
+f.close()
