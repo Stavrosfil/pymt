698ea5e2f0c732fbac87179e4053bfa4ca17e017
Stavros Filosidis <stavrosfil@gmail.com>
2019-11-28 01:42:36 +0200

    Option to initialize Stop with HTML

--- scripts/Stop.py
+++ scripts/Stop.py
@@ -1,10 +1,31 @@
+import oasth_parsing as oasth_parser
+
+
 class Stop:
 
-    def __init__(self, stop_id=-1, description='', buses=[], lines=[]):
-        self.stop_id = stop_id
-        self.description = description
-        self.buses = buses
-        self.lines = lines
+    def __init__(self, payload=None, stop_id=-1, description='', buses=[], lines=[]):
+
+        if payload is not None:
+            """
+            Initialize a stop object with a beautifulSoup html one.
+
+            Arguments:
+                stop_html {BeautifulSoup} -- [the payload received from the server]
+            """
+
+            stop = oasth_parser.parse_stop(payload, stop_id)
+
+            # Deep copy
+            self.stop_id = stop.stop_id
+            self.description = stop.description
+            self.buses = stop.buses
+            self.lines = stop.lines
+
+        else:
+            self.stop_id = stop_id
+            self.description = description
+            self.buses = buses
+            self.lines = lines
 
     def add_bus(self, bus):
         self.buses.append(bus)
