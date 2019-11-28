ba96e4e4b09ee200cfb111a02c2e69ee9a076acc
Stavros Filosidis <stavrosfil@gmail.com>
2019-11-28 03:16:54 +0200

    Open and close files correctly, use json and relative paths to data files

--- scripts/stop_analyzer_async.py
+++ scripts/stop_analyzer_async.py
@@ -7,34 +7,25 @@ import async_requests
 import Stop as Stop
 sys.path.append('/home/stavrosfil/repos/pymt/scripts/modules')
 
-# import time
 
+DATA_FOLDER = Path("data")
 
-data_folder = Path("data")
+stop_ids = []
 
-od = data_folder / "stop_info_async.json"
+with open(DATA_FOLDER / "oasth_stops.json", "r") as f:
 
-of = open(od, "a")
-
-# of = open("/data/stop_info_async.json", "a")
-
-with open('oasth_stops.json') as f:
-
-    stop_ids = []
-
-    data = json.load(f)
-
-    # TODO: Convert oasth_stops.txt file to a json one for better data handling.
-    for d in data:
-        # print(f'Stop ID: { str(stop_id) }')
+    for d in json.load(f):
         stop_ids.append(d["stop_id"])
+        # print(f'Stop ID: { str(stop_id) }')
+
+    f.close()
+
+
+with open(DATA_FOLDER / "stop_info_async.json", "a") as of:
 
     # stop_ids = stop_ids[0:50]
 
     responses = async_requests.get_stops(stop_ids)
-
-    # ---------------------------------- PARSING --------------------------------- #
-
     of.write('[')
 
     for response, stop_id in zip(responses, stop_ids):
@@ -42,16 +33,11 @@ with open('oasth_stops.json') as f:
         stop = Stop.Stop(response, stop_id)
 
         if(stop is not None):
-
             of.write('\n')
-
             stop_json = {'stop_id': stop.stop_id,
                          'buses': [b.__dict__ for b in stop.buses]}
-
             json.dump(stop_json, of, indent=2, ensure_ascii=False)
-
             if (stop_id != stop_ids[-1]):
                 of.write(',')
 
-f.close()
-of.close()
+    of.close()
