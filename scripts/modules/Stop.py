d94e91e42748672e8ca5280e40789a957ad1334c
Stavros Filosidis <stavrosfil@gmail.com>
2019-12-09 18:19:29 +0200

    Add parameters to class

--- scripts/modules/Stop.py
+++ scripts/modules/Stop.py
@@ -3,19 +3,20 @@ import oasth_parsing as oasth_parser
 
 class Stop:
 
-    def __init__(self, payload=None, url=None, stop_id=-1, name='', buses=[], lines=[]):
+    def __init__(self, payload=None, url=None, stop_id=-1, name='', buses=[], lines=[], params={}):
 
         if payload is not None:
             """
-            Initialize a stop object with a beautifulSoup html one.
+            Initialize a stop object with a beautifulSoup html one, using the telematics data received.
 
             Arguments:
-                stop_html {BeautifulSoup} -- [the payload received from the server]
+                payload {BeautifulSoup} -- [the payload received from the xhr request]
             """
 
             stop = oasth_parser.parse_stop(self, payload, stop_id)
 
         else:
+            self.params = params
             self.url = url
             self.stop_id = stop_id
             self.name = name
