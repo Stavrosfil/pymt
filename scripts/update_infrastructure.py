9ca55727846a71775e6ddfb502730674558d89e8
Stavros Filosidis <stavrosfil@gmail.com>
2020-01-12 22:48:18 +0200

    - Use main() - Extract UIDs for lines - use methods for each task - Use flashdb instead of flashall - Update naming scheme - Use unique databases for each function

--- scripts/update_infrastructure.py
+++ scripts/update_infrastructure.py
@@ -4,45 +4,58 @@ import redis_save_stops
 import redis
 
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+def main():
+    SELECTED_LINES = ['01N', '02K', '03K']
+    stops = get_line_stops(SELECTED_LINES)
+    print(stops)
 
 
-# UPDATE LINES
-def update():
-    r.flushall()
+def get_line_stops(selected_lines, db=0):
+
+    selected_stops = []
+
+    # Initialize Redis client object
+    r = redis.Redis(host='localhost', port=6379, db=db)
+
+    # Load lines from Redis cache into dictionary
+    selected_lines_uids = [int(l) for l in r.hmget('lines', selected_lines)]
+    # print(selected_lines_uids)
+
+    for line_uid in selected_lines_uids:
+        for direction in (1, 2):
+            lsuid = 'line{}:stops:direction{}'.format(line_uid, direction)
+            stops = [int(s) for s in r.hgetall(lsuid).keys()]
+            selected_stops.extend(stops)
+
+    return selected_stops
+
+# ---------------------------------------------------------------------------- #
+#                             UPDATE INFRASTRUCTURE                            #
+# ---------------------------------------------------------------------------- #
+
+
+def redis_update_infrastructure(db=0):
+
+    # Initialize Redis client object
+    r = redis.Redis(host='localhost', port=6379, db=db)
+
+    # Clean all lines in db
+    r.flushdb()
+
+    # Scrape all indivudual lines and save them
+    # TODO: Do not use saving by default inside scrape_lines
     scrape_lines.scrape_lines()
+
+    # Load all line UIDs in memory
     lines = [l.decode('utf-8') for l in r.hvals('lines')]
+
+    # Use line UID to scrape individual lines
     for line in lines:
         scrape_line.scrape_line(line)
 
-
-# update()
-
-
-# lines = [int(l) for l in r.hvals('lines')]
-lines = {}
-for i, j in zip(r.hkeys('lines'), r.hvals('lines')):
-    lines[i.decode('utf-8')] = int(j)
-
-# lines = r.lrange('lines', 0, -1)
-print(list(lines.keys()))
-# print(list(lines.values()))
+    # Save database to memory
+    r.save()
 
 
-selected_lines = ['01N', '02K', '03K']
-selected_stops = []
-for line in selected_lines:
-    # print(lines[line])
-    for direction in (1, 2):
-        lsuid = 'line{}:direction{}:stops'.format(lines[line], direction)
-        stops = [int(s) for s in r.hgetall(lsuid).keys()]
-        print(stops)
-        selected_stops.extend(stops)
-
-
-r.save()
-
-# print(r.hgetall('s819')[b'name'])
-# print(r.hgetall('ls102K'))
-
-print(selected_stops)
+if __name__ == "__main__":
+    main()
