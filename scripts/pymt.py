e087b22bf1ab3334a6f1ab492008ead048e5eb86
Stavros Filosidis <stavrosfil@gmail.com>
2020-02-08 23:34:41 +0200

    Use logger, save >= WARNING levels to logs

--- scripts/pymt.py
+++ scripts/pymt.py
@@ -2,15 +2,16 @@ import async_requests
 import time
 from influxdb import InfluxDBClient
 import redis_operations as ro
-from default_logger import logging
-# import logging
+import default_logger
 import sys
+
+logger = default_logger.logger
 
 
 def main():
 
     # SELECTED_LINES = ro.get_all_lines()[:50]
-    logging.info("Initializing redis client...")
+    logger.info("Initializing redis client...")
 
     SELECTED_LINES = ['01N', '01X', '02K', '03K', '10', '07', '31']
     INFLUX_URI = 'localhost'
@@ -18,32 +19,32 @@ def main():
     INFLUX_DB = 'bus_arrivals'
 
     try:
-        logging.info("Loading stops for lines: {}".format(SELECTED_LINES))
-        logging.info("Initializing redis client...")
+        logger.info("Loading stops for lines: {}".format(SELECTED_LINES))
+        logger.info("Initializing redis client...")
         stops = ro.get_line_stops(SELECTED_LINES)
-        logging.info("Stops loaded from Redis: {}".format(len(stops)))
+        logger.info("Stops loaded from Redis: {}".format(len(stops)))
     except Exception as e:
-        logging.exception("Failed to fetch data from Redis client: {}".format(e))
+        logger.exception("Failed to fetch data from Redis client: {}".format(e))
         sys.exit()
 
     try:
-        logging.info("Initializing InfluxDB client: {}:{}".format(INFLUX_URI, INFLUX_PORT))
+        logger.info("Initializing InfluxDB client: {}:{}".format(INFLUX_URI, INFLUX_PORT))
         influx_client = InfluxDBClient(INFLUX_URI, INFLUX_PORT)
-        logging.info("Client initialized successfully")
+        logger.info("Client initialized successfully")
     except Exception as e:
-        logging.exception("Failed to initialize InfluxDB client: {}".format(e))
+        logger.exception("Failed to initialize InfluxDB client: {}".format(e))
         sys.exit()
 
     try:
         dbs = influx_client.get_list_database()
         if {'name': 'bus_arrivals'} not in dbs:
-            logging.info("Creating database: {}".format(INFLUX_DB))
+            logger.info("Creating database: {}".format(INFLUX_DB))
             influx_client.create_database(INFLUX_DB)
-        logging.info("Switching to database: {}".format(INFLUX_DB))
+        logger.info("Switching to database: {}".format(INFLUX_DB))
         influx_client.switch_database(INFLUX_DB)
-        logging.info("Successfully switched to: {}".format(INFLUX_DB))
+        logger.info("Successfully switched to: {}".format(INFLUX_DB))
     except Exception as e:
-        logging.exception("Could not switch to {}: {}".format(INFLUX_DB, e))
+        logger.exception("Could not switch to {}: {}".format(INFLUX_DB, e))
         sys.exit()
 
     start_loop(stops, influx_client)
@@ -58,11 +59,11 @@ def start_loop(stops, influx_client):
         timer_requests = time.time()
 
         try:
-            logging.info("Quering async requests to OASTh...")
+            logger.info("Quering async requests to OASTh...")
             responses = async_requests.get_stops([s.uid for s in stops])
-            logging.info("Received {} responses in {} seconds".format(len(responses), time.time() - timer_requests))
+            logger.info("Received {} responses in {} seconds".format(len(responses), time.time() - timer_requests))
         except Exception as e:
-            logging.exception("There was an exception while getting data from the server: {}".format(e))
+            logger.exception("There was an exception while getting data from the server: {}".format(e))
 
         timer_parsing = time.time()
 
@@ -71,9 +72,9 @@ def start_loop(stops, influx_client):
                 try:
                     stop.update(response)
                 except Exception as e:
-                    logging.exception("There was an exception while parsing received response: {}".format(e))
+                    logger.exception("There was an exception while parsing received response: {}".format(e))
 
-        logging.info("Parsed responses in: {} seconds".format(time.time() - timer_parsing))
+        logger.info("Parsed responses in: {} seconds".format(time.time() - timer_parsing))
 
         saveToInflux(influx_client, stops)
 
@@ -82,7 +83,7 @@ def start_loop(stops, influx_client):
 
 def saveToInflux(client, stops):
 
-    logging.info("Writing to InfluxDB...")
+    logger.info("Writing to InfluxDB...")
     time2 = time.time()
 
     json_body = []
@@ -112,9 +113,9 @@ def saveToInflux(client, stops):
     try:
         client.write_points(json_body)
         # print(json_body)
-        logging.info("Successfully written in {} seconds".format(time.time() - time2))
+        logger.info("Successfully written in {} seconds".format(time.time() - time2))
     except Exception as e:
-        logging.exception("There was an error writing to the database: {}".format(e))
+        logger.exception("There was an error writing to the database: {}".format(e))
 
 
 if __name__ == '__main__':
