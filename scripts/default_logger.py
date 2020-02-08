8bb7e2d8587d02560a9864fa9a99c9c6e1410e12
Stavros Filosidis <stavrosfil@gmail.com>
2020-02-08 23:33:58 +0200

    Fix logger file not displaying INFO severety levels

--- scripts/default_logger.py
+++ scripts/default_logger.py
@@ -1,22 +1,22 @@
+
 # logging_example.py
 
 import logging
 
 # Create a custom logger
-logger = logging.getLogger('pymt')
+logger = logging.getLogger(__name__)
 
 # Create handlers
 c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler('pymt.log', 'a')
-c_handler.setLevel(logging.INFO)
-f_handler.setLevel(logging.INFO)
+f_handler = logging.FileHandler('pymt.log')
+
+# Set severety logging level
+logger.setLevel(logging.INFO)
+f_handler.setLevel(logging.WARNING)
 
 # Create formatters and add it to handlers
-# c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-# f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-c_format = f_format = logging.Formatter('%(asctime)s - %(process)d - %(levelname)s - %(name)s - %(message)s',
-                                        datefmt='%d-%b-%y %H:%M:%S',)
-
+# c_format = f_format = logging.Formatter('%(asctime)s - %(process)d - %(levelname)s - %(name)s - %(message)s')
+c_format = f_format = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 c_handler.setFormatter(c_format)
 f_handler.setFormatter(f_format)
 
