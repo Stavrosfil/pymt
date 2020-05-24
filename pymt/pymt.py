d63fe70cb3ab27f63697e802c0c8ebad5181c08a
Stavros Filosidis <stavrosfil@gmail.com>
2020-05-24 16:06:54 +0300

    Zero value route requests are not sent to server - 50% decrease for sundays
    Signed-off-by: Stavros Filosidis <stavrosfil@gmail.com>

--- pymt/pymt.py
+++ pymt/pymt.py
@@ -13,15 +13,16 @@ influx_client = infl.InfluxClient()
 @pymt.helpers.infl.performance(influx_client)
 @pymt.helpers.metadata.timer("Getting bus telematics...")
 def get_buses(lines):
-    # Get telematics
-    route_telematics = api.get_async([l.get_telematics_url(day) for day in range(2) for l in lines])
+    routes = [l.get_telematics_url(day) for day in range(2) for l in lines if day != 0]
+    logger.warning(f"Route urls to request: {len(routes)}")
+    route_telematics = api.get_async(routes)
     return [model.Bus(b) for route in route_telematics for b in route if b]
 
 
 def run():
     today = datetime.datetime.today().weekday()
     days = [today * 2, today * 2 + 1]
-    logger.debug(days)
+    logger.info(f"Days: {days}")
     loaded_lines = mon.load_lines(days)
 
     while True:
@@ -30,6 +31,7 @@ def run():
         if new_day != today:
             days = [new_day * 2, new_day * 2 + 1]
             loaded_lines = mon.load_lines(days)
+            logger.warning(f"Days: {days}")
 
         for l in loaded_lines:
             logger.debug("Line: {}".format(l.name))
